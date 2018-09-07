d85f3127ac3a116d00d9da51754c9b1797a1b5b0
sai jayanth <jayanth833@gmail.com>
2018-09-07 23:22:37 +0530

    first python code

---
+++ Introduction.py
@@ -0,0 +1,22 @@
+import string;
+import math;
+import calendar;
+import datetime;
+alpha = ['a','b','c'];
+print('a' or 'b' in alpha);
+if 'h' in alpha:
+    print(True);
+else:
+    print(False);
+c = 0
+while c==0:
+    c = c+1;
+    print(c)
+    break;
+
+name = 'yout:ube'
+print(name.split(':')[0])
+
+name = name.replace('y','p');
+d = datetime
+print (d.timezone)
